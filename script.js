69f8f41667f9c21f926542fe2428708c17cd5d7b
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-16 14:11:00 +0100

    MAJ

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const nomCommercial = document.getElementById("nomCommercial");
 let boutonAppel = document.getElementById("boutonAppel");
-
 const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
 
 fetch(apiUrl)
