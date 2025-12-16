294f66211092fcdc134b537dfc29a96aab78f5ed
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-16 12:29:50 +0100

    Ajouts id et MAJ JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let API = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
+const nomCommercial = document.getElementById("nomCommercial");
+let boutonAppel = document.getElementById("boutonAppel");
 
-// fetch(API)
+const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    nomCommercial.textContent = data.nomCommercial;
+    boutonAppel.textContent = data.texteAppelAction;
+});
+
+// function afficherProduits(){
+
+// }
