71978728cf861ae1a2824d2c5d77072c848d3c79
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-16 16:03:10 +0100

    MAJ

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// variables
 const nomCommercial = document.getElementById("nomCommercial");
 let boutonAppel = document.getElementById("boutonAppel");
+let phraseAccroche = document.getElementById("phraseAccroche");
+let boutonExplorer = document.getElementById("boutonExplorer");
+let sectionTemoignages = document.getElementById("temoignages")
+//API URL
 const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
 
 fetch(apiUrl)
@@ -7,9 +12,30 @@ fetch(apiUrl)
 .then(data => {
     console.log(data);
     nomCommercial.textContent = data.nomCommercial;
-    boutonAppel.textContent = data.texteAppelAction;
+    boutonAppel.textContent = data.texteAppelAction; //bouton appel texte
+    phraseAccroche.textContent = data.phraseAccroche;
+    boutonExplorer.textContent = data.texteAppelAction;
+    afficherTemoignages(data);
 });
 
-// function afficherProduits(){
+function afficherTemoignages(e){
+    e.temoignages.forEach(temoin => {
+        //container de chaque card
+        let temoignageCard = document.createElement("div");
 
-// }
+        let prenom = document.createElement("p");
+        prenom.textContent = temoin.prenom;
+        temoignageCard.appendChild(prenom);
+
+        
+        //type experience
+        let typeExperience = document.createElement("p");
+        typeExperience.textContent = temoin.typeExperience;
+        console.log(e.typeExperience);
+        temoignageCard.appendChild(typeExperience);
+        temoignages.appendChild(temoignageCard);
+        
+    });
+
+    
+}
